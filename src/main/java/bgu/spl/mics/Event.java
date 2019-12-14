66856717e59f69720751477d7a9e3da56ea7579b
zatlawy@wincs.cs.bgu.ac.il <zatlawy@post.bgu.ac.il>
2019-12-14 10:50:16 +0200

    added events

--- src/main/java/bgu/spl/mics/Event.java
+++ src/main/java/bgu/spl/mics/Event.java
@@ -7,6 +7,4 @@ package bgu.spl.mics;
  * When sending an event, it will be received only by a single subscriber in a
  * Round-Robin fashion.
  */
-public interface Event<T> extends Message {
-
-}
+public interface Event<T> extends Message {}
