394790bd1a76498d8830c2fe39809bfed8eb36df
Nadav Kaduri <kadurin@post.bgu.ac.il>
2019-12-19 22:13:32 +0200

    printToFile

--- src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
@@ -14,6 +14,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * <p>
  * You can add ONLY private fields and methods to this class as you see fit.
  */
+
 public class Diary {
 	private List<Report> reports;
 	private AtomicInteger total;
